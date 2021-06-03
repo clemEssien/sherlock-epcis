b450909d614a3d32819593467ecaa7789d796bcb
ryanoostland <ryan.oostland@precise-soft.com>
2021-06-03 10:07:51 -0400

    Added regex to validate URI

--- JSONDeserialization/epcis_event.py
+++ JSONDeserialization/epcis_event.py
@@ -1,4 +1,6 @@
 import datetime
+import re
+
 
 class URI():
     """ Provides a class for URI objects
@@ -13,10 +15,14 @@ class URI():
     """
     def __init__(self, input_str:str):
         """Creates a new URI instance from the given string"""
-        uri = input_str.split(':')
-        self._prefix = "{}:{}:{}:{}".format(uri[0],uri[1],uri[2],uri[3])
-        self._epc_scheme = uri[3]
-        self._value = uri[4]
+        self.uri = input_str
+        if re.search("[a-z]+:[a-z]+:[a-z]+:[a-z]+:[a-z0-9.*]+",input_str) is not None:
+            uri = input_str.split(':')
+            self.prefix = "{}:{}:{}:{}".format(uri[0],uri[1],uri[2],uri[3])
+            self.epc_scheme = uri[3]
+            self.value = uri[4]
+    def __str__(self):
+        return self.uri
 
 #TODO: flesh out event object and replace instances of str where that string represents a URI
 class EPCISEvent():
@@ -30,17 +36,17 @@ class EPCISEvent():
         self._event_type: str = ''
         self._event_time = datetime.datetime(1,1,1)
         self._event_timezone_offset = datetime.timezone(datetime.timedelta(hours=0))
-        self._epc_list: list[str] = []
-        self._parent_id: str = ''
-        self._child_epc_list: list[str] = []
-        self._input_epc_list: list[str] = []
-        self._output_epc_list: list[str] = []
-        self._xform_id: str = ''
+        self._epc_list: list[URI] = []
+        self._parent_id: URI = ''
+        self._child_epc_list: list[URI] = []
+        self._input_epc_list: list[URI] = []
+        self._output_epc_list: list[URI] = []
+        self._xform_id: URI = ''
         self._action: str = ''
-        self._business_step: str = ''
-        self._disposition: str = ''
-        self._business_location: str = ''
-        self._read_point: str = ''
+        self._business_step: URI = ''
+        self._disposition: URI = ''
+        self._business_location: URI = ''
+        self._read_point: URI = ''
         self._instance_lot_master_data: dict = {}
         self._quantity_list: list[dict] = []
         self._child_quantity_list: list[dict] = []
@@ -50,6 +56,35 @@ class EPCISEvent():
         self._source_list: list[dict] = []
         self._destination_list: list[dict] = []
 
+        @property
+        def event_type(self) -> str:
+            """event_type"""
+            return self._event_type
 
+        @event_type.setter
+        def event_type(self, value: str):
+            self._event_type = value
 
+        @property
+        def event_time(self) -> datetime.datetime:
+            """event_time"""
+            return self._event_time
 
+        @event_time.setter
+        def event_time(self, value: datetime.datetime):
+            self._event_time = value
+ 
+uri1 = URI("abc")
+uri2 = URI("urn:epc:class:lgtin:0614141.077777.987")
+uri3 = URI("urn:epcglobal:cbv:bizstep:commissioning")
+
+print(uri1.uri)
+print(uri1.prefix)
+
+print(uri2.value)
+print(uri2.prefix)
+print(uri2.epc_scheme)
+
+print(uri3.value)
+print(uri3.prefix)
+print(uri3.epc_scheme)
