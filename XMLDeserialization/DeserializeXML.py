11fd9da22cdffc7697821f2e26a48136607ba1cc
Helina <helina.solmn@precise-soft.com>
2021-06-04 11:52:17 -0400

    fixed if statement

--- XMLDeserialization/DeserializeXML.py
+++ XMLDeserialization/DeserializeXML.py
@@ -7,8 +7,9 @@ def main() :
     tree = ET.parse("XMLDeserialization/GS1StandardExample1.xml")
     root = tree.getroot()
 
-    if root.tag == "EPCISDocument" :        # doesnt work but concept
-        print("worked")
+    if root.tag != "{urn:epcglobal:epcis:xsd:1}EPCISDocument" :       
+        print("Not EPCISDocument")
+        return
 
     for child in root.iter('EventList') :
         for event in child :
