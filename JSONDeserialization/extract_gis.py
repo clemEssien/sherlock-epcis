60fe8306fbc95fad0aac658cd9fd4e9d53922052
Clement <clement.essien@precise-soft.com>
2021-06-10 10:03:47 -0400

    commit before pulling updated event object

--- JSONDeserialization/extract_gis.py
+++ JSONDeserialization/extract_gis.py
@@ -4,7 +4,12 @@ import datetime
 
 def type_check(value):
     if isinstance(value, datetime.datetime):
-        return value.isoformat("T").replace("+00:00", "") + "Z"
+        try:
+            return value.isoformat("T").replace("+00:00", "") + "Z"
+        except:
+            print("Attribute ",value," not converted to iso format")
+        finally:
+            return value
     elif isinstance(value, datetime.date):
         return value.strftime("%Y-%m-%d")
     elif isinstance(value, datetime.time):
@@ -24,19 +29,13 @@ def map_from_epcis(epcis_event_obj,epcis_json):
         obj: Any
             The event object
     """
-    epcis_event_list = []
     if epcis_json is None:
         return None
 
     with open('schema.json') as f:
         schema_doc = json.load(f)
 
-    for attr in schema_doc['attr_key_mapping'].keys():
-        if attr in epcis_json.keys():
-            value = type_check(epcis_json[schema_doc['attr_key_mapping'][attr]])
-            setattr(epcis_event_obj, attr, value)
-            epcis_event_list.append(epcis_event_obj)
-    return epcis_event_list
+
 
 # driver code
 def main():
@@ -50,7 +49,7 @@ def main():
 
     for event in events:
         epcis_event_obj = epcis_event.EPCISEvent()
-        event_objects = map_from_epcis(epcis_event_obj, event)
+        map_from_epcis(epcis_event_obj, event)
 
 if __name__ == '__main__':
     main()
