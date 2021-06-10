7aa9bb701dd488af3c0bdba24c749d78b20e26eb
Helina <helina.solmn@precise-soft.com>
2021-06-10 13:28:26 -0400

    added python black formatter

--- XMLDeserialization/DeserializeXMLRecursive.py
+++ XMLDeserialization/DeserializeXMLRecursive.py
@@ -1,35 +1,39 @@
-#Author: Kevin Zong and Helina Solomon
-#Last Modified: June 10th, 2021
-#Script to deserialize standard EPCIS documents in XML form recursively in order to avoid the <extension> tags
+# Author: Kevin Zong and Helina Solomon
+# Last Modified: June 10th, 2021
+# Script to deserialize standard EPCIS documents in XML form recursively in order to avoid the <extension> tags
 import xml
 import pprint
 import xml.etree.ElementTree as ET
 
-def main():
-    parseXML('XMLDeserialization/GS1StandardExample4.xml')
 
-def parseXML(filename) :
+def main():
+    parseXML("XMLDeserialization/GS1StandardExample4.xml")
+
+
+def parseXML(filename):
     eventDicts = []
     tree = ET.parse(filename)
     root = tree.getroot()
 
-    if root.tag != "{urn:epcglobal:epcis:xsd:1}EPCISDocument" :                     #Check for the EPCISDocument tag 
+    if (
+        root.tag != "{urn:epcglobal:epcis:xsd:1}EPCISDocument"
+    ):  # Check for the EPCISDocument tag
         print("Not EPCISDocument")
         return
-    eventDicts = []                                                                 #List of Event dicts
-    for list in root.iter('EventList'):
-        for event in list:                                                          #Iterate through each event 
+    eventDicts = []  # List of Event dicts
+    for list in root.iter("EventList"):
+        for event in list:  # Iterate through each event
             myDict = {}
-            if(event.tag == "extension") :                                          #Check for extension tag on Event
-                for subEvent in event :                                             #Iterate through each event in extension
-                    #print(subEvent.tag)
+            if event.tag == "extension":  # Check for extension tag on Event
+                for subEvent in event:  # Iterate through each event in extension
+                    # print(subEvent.tag)
                     myDict["isA"] = subEvent.tag
-                    myDict.update(parseTag(subEvent))                               
+                    myDict.update(parseTag(subEvent))
                     tempDict = myDict.copy()
-                    eventDicts.append(tempDict)                                     
-            else :
-                #print(event.tag)
-                myDict["isA"] = event.tag            
+                    eventDicts.append(tempDict)
+            else:
+                # print(event.tag)
+                myDict["isA"] = event.tag
                 myDict.update(parseTag(event))
                 tempDict = myDict.copy()
                 eventDicts.append(tempDict)
@@ -38,29 +42,45 @@ def parseXML(filename) :
     for event in eventDicts:
         pp.pprint(event)
         print("")
+
+
 def parseTag(node):
     nodeDict = {}
-    if(node.tag == 'epcList' or node.tag == 'childEPCs' or node.tag == 'inputEPCList' or node.tag == 'outputEPCList') : #epc lists
-        #print('parsing an epcList')
+    if (
+        node.tag == "epcList"
+        or node.tag == "childEPCs"
+        or node.tag == "inputEPCList"
+        or node.tag == "outputEPCList"
+    ):  # epc lists
+        # print('parsing an epcList')
         uriList = []
-        for epc in node :
+        for epc in node:
             uriList.append(epc.text)
-        if(len(uriList) > 0):    
+        if len(uriList) > 0:
             nodeDict[node.tag] = uriList
-    elif(node.tag == 'readPoint' or node.tag == 'bizLocation') :
-        #print('parsing readPoint or bizLocation')
+    elif node.tag == "readPoint" or node.tag == "bizLocation":
+        # print('parsing readPoint or bizLocation')
         for id in node:
             nodeDict[node.tag] = parseTag(id)
-    elif(node.tag == 'bizTransactionList' or node.tag == 'sourceList' or node.tag == 'destinationList') : #typed lists
+    elif (
+        node.tag == "bizTransactionList"
+        or node.tag == "sourceList"
+        or node.tag == "destinationList"
+    ):  # typed lists
         dictList = []
         for listElement in node:
             transactionDict = {}
-            transactionDict['type'] = listElement.get('type')
+            transactionDict["type"] = listElement.get("type")
             transactionDict[listElement.tag] = listElement.text
             tempDict = transactionDict.copy()
             dictList.append(tempDict)
         nodeDict[node.tag] = dictList
-    elif(node.tag == 'quantityList' or node.tag == 'childQuantityList' or node.tag == 'inputQuantityList' or node.tag == 'outputQuantityList') :
+    elif (
+        node.tag == "quantityList"
+        or node.tag == "childQuantityList"
+        or node.tag == "inputQuantityList"
+        or node.tag == "outputQuantityList"
+    ):
         dictList = []
         for listElement in node:
             dictList.append(parseTag(listElement))
@@ -68,11 +88,12 @@ def parseTag(node):
     else:
         if len(node):
             for child in node:
-                #print('parsing ' +child.tag)
+                # print('parsing ' +child.tag)
                 nodeDict.update(parseTag(child))
         else:
             nodeDict[node.tag] = node.text
     return nodeDict
 
-if __name__ == '__main__' :
+
+if __name__ == "__main__":
     main()
