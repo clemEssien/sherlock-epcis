e1e8228a44cab60f5103a0d3b2eb5153f32362d6
Clement <clement.essien@precise-soft.com>
2021-06-10 15:52:46 -0400

    added event type checking

--- JSONDeserialization/extract_gis.py
+++ JSONDeserialization/extract_gis.py
@@ -15,7 +15,7 @@ def type_check(value):
     elif isinstance(value, datetime.time):
         return value.strftime("%H:%M:%S.%f")
     elif isinstance(value, dict):
-        return value[id]
+        return value["id"]
     elif isinstance(value, int) or isinstance(value, float) or isinstance(value, str):
         return value
 
@@ -35,11 +35,30 @@ def map_from_epcis(epcis_event_obj,epcis_json):
     with open('schema.json') as f:
         schema_doc = json.load(f)
 
+    for attr in epcis_event_obj.__dict__.keys():
+        if attr in schema_doc['attr_key_mapping'].keys():
+            try:
+                instvar = getattr(epcis_event_obj, attr)
+                value = type_check(epcis_json[schema_doc['attr_key_mapping'][attr]])
+                setattr(epcis_event_obj, attr, value)
+            except:
+                a = 3
+                # print(attr)
+    print(epcis_event_obj)
+
+
 
 
 # driver code
 def main():
-    event_objects = []
+    event_types = {
+        "ObjectEvent": epcis_event.ObjectEvent(),
+        "AggregationEvent": epcis_event.AggregationEvent(),
+        "QuantityEvent": epcis_event.QuantityEvent(),
+        "TransactionEvent": epcis_event.TransactionEvent(),
+        "TransformationEvent": epcis_event.TransformationEvent(),
+    }
+
     with open('GS1StandardExample1.json') as f:
         epcis_doc = json.load(f)
         if epcis_doc['isA'] != 'EPCISDocument':
@@ -48,7 +67,8 @@ def main():
         events = epcis_doc['epcisBody']['eventList']
 
     for event in events:
-        epcis_event_obj = epcis_event.EPCISEvent()
+        event_type = (event['isA'])
+        epcis_event_obj = event_types[event_type]
         map_from_epcis(epcis_event_obj, event)
 
 if __name__ == '__main__':
