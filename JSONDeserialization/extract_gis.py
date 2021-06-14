3ed4d7696546a1f6158290220dc941043818fbba
Clement <clement.essien@precise-soft.com>
2021-06-14 12:06:34 -0400

    Fixed URI attributes not being set

--- JSONDeserialization/extract_gis.py
+++ JSONDeserialization/extract_gis.py
@@ -2,22 +2,77 @@ import json
 import epcis_event
 import datetime
 
-def type_check(value):
+
+def read_uri(uri):
+    if type(uri) == dict:
+        return uri['id']
+    return epcis_event.URI(uri)
+
+def is_primitive(value) -> bool:
+    """
+    Returns True if the type is a primitive value
+
+    Primitive values include str, int, float, datetime, date, and time.
+    """
     if isinstance(value, datetime.datetime):
-        try:
-            return value.isoformat("T").replace("+00:00", "") + "Z"
-        except:
-            print("Attribute ",value," not converted to iso format")
-        finally:
-            return value
+        return True
     elif isinstance(value, datetime.date):
-        return value.strftime("%Y-%m-%d")
+        return True
     elif isinstance(value, datetime.time):
-        return value.strftime("%H:%M:%S.%f")
-    elif isinstance(value, dict):
-        return value["id"]
-    elif isinstance(value, int) or isinstance(value, float) or isinstance(value, str):
-        return value
+        return True
+    elif isinstance(value, int) or isinstance(value, float):
+        return True
+    elif isinstance(value, str):
+        return True
+    else:
+        return False
+
+def attr_type_check(instvar, data, attr):
+    if isinstance(instvar, str):
+        value = data
+    if isinstance(instvar, list):
+        value = data
+        arr_out = []
+
+        for val in data:
+            if is_primitive(val):
+                arr_out.append(val)
+            else:
+                arr_out.append(str(val))
+        return arr_out
+
+    if isinstance(instvar, epcis_event.URI):
+        value = read_uri(data)
+
+    elif isinstance(instvar, dict):
+        print('dict')
+        a = 1
+        dict_out = {}
+        keys = data.keys()
+        print(keys)
+        if len(keys):
+            for key in keys:
+                if is_primitive(key):
+                    dict_out[key] = data
+                else:
+                    dict_out[key] = str(data)
+
+        return dict_out
+
+    elif isinstance(instvar, datetime.date):
+        try:
+            value = datetime.datetime.strptime(data, "%Y-%m-%d")
+            value = datetime.date(value.year, value.month, value.day)
+        except:
+            value = data
+    elif isinstance(instvar, datetime.datetime):
+        try:
+            value = datetime.date.fromisoformat(data)
+        except:
+            value = data
+    elif isinstance(instvar, datetime.timezone):
+        value = data
+    return value
 
 
 def map_from_epcis(epcis_event_obj,epcis_json):
@@ -35,16 +90,36 @@ def map_from_epcis(epcis_event_obj,epcis_json):
     with open('schema.json') as f:
         schema_doc = json.load(f)
 
-    for attr in epcis_event_obj.__dict__.keys():
-        if attr in schema_doc['attr_key_mapping'].keys():
-            try:
-                instvar = getattr(epcis_event_obj, attr)
-                value = type_check(epcis_json[schema_doc['attr_key_mapping'][attr]])
-                setattr(epcis_event_obj, attr, value)
-            except:
-                a = 3
-                # print(attr)
+    for attr in list(epcis_event_obj.__dict__.keys()):
+        attr = attr[1:]
+
+        try:
+
+            instvar = getattr(epcis_event_obj, attr)
+            value = epcis_json[schema_doc['attr_key_mapping'][attr]]
+            formated_value = attr_type_check(instvar, value, attr)
+            #default_value = type_check(epcis_json[schema_doc['attr_key_mapping'][attr]])
+            # print(attr,formated_value)
+            setattr(epcis_event_obj, attr, formated_value)
+
+        except Exception as e:
+            a = 1
+            # print("error: ",attr, e)
+
+    epcis_json_keys = epcis_json.keys()
+    schema_values = schema_doc["attr_key_mapping"].values()
+    ext_keys = (set(epcis_json_keys) - set(schema_values))
+    ext_dict = {}
+    for k in ext_keys:
+        ext_dict[k] = epcis_json[k]
+    epcis_event_obj.extensions.append(ext_dict)
+
+    #search for keys in epcis_json but not in schema doc and set in the extension
+
+
+
     print(epcis_event_obj)
+    print("**************")
 
 
 
