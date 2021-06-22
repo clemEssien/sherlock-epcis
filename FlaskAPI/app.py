04da4295c5ed0a99629c053d40ba70b7c974ac19
Charlie Shou <charlie.shou@precise-soft.com>
2021-06-22 15:25:51 -0400

    /xml post endpoint

--- FlaskAPI/app.py
+++ FlaskAPI/app.py
@@ -3,6 +3,7 @@ from flask_classful import FlaskView, route
 from neo4j import GraphDatabase
 
 import os, sys
+import xml.etree.ElementTree as ET
 
 currentdir = os.path.dirname(os.path.realpath(__file__))
 parentdir = os.path.dirname(currentdir)
@@ -20,11 +21,11 @@ driver = GraphDatabase.driver(uri=URI, auth=(USER, PASS))
 app = Flask(__name__)
 
 event_types = {
-    "ObjectEvent": epc.ObjectEvent(),
-    "AggregationEvent": epc.AggregationEvent(),
-    "QuantityEvent": epc.QuantityEvent(),
-    "TransactionEvent": epc.TransactionEvent(),
-    "TransformationEvent": epc.TransformationEvent(),
+    "ObjectEvent": epc.ObjectEvent,
+    "AggregationEvent": epc.AggregationEvent,
+    "QuantityEvent": epc.QuantityEvent,
+    "TransactionEvent": epc.TransactionEvent,
+    "TransformationEvent": epc.TransformationEvent,
 }
 
 
@@ -38,11 +39,11 @@ class JSONView(FlaskView):
             results = session.run(q).data()
         return jsonify(results)
 
-    @route("/", methds=["POST"])
+    @route("/", methods=["POST"])
     def post(self):
         """POST an JSON EPCIS event to add to db"""
         epcis_json = request.get_json()
-        event = event_types[epcis_json["isA"]]
+        event = event_types[epcis_json["isA"]]()
         ex_json.map_from_epcis(event, epcis_json)
         q = "create (:Event{eventTime: datetime($eventTime), eventTimeZoneOffset: $eventTimeZoneOffset})"
         qmap = {
@@ -59,20 +60,62 @@ class JSONView(FlaskView):
 class XMLView(FlaskView):
     route_base = "/xml"
 
-    @route("/", method=["GET"])
+    @route("/", methods=["GET"])
     def index(self):
         pass
 
-    @route("/", method=["POST"])
+    @route("/", methods=["POST"])
     def post(self):
         """
+        IN PROGRESS
         Posts XML EPCIS event to add to db
 
-        Request Body: {
-            xml: str *string of xml data
-        }
+        Request Body: 
+            xml data of epcis
+
+        Error Codes:
+            400: Bad request
+
+        On Success (200):
+            {
+                success: true
+                events: any
+            }
         """
-        epcis_xml = request.get_json();
+        epcis_xml = str(request.get_data(), "utf-8") 
+        
+        root = ET.fromstring(epcis_xml)
+
+        if root.tag != "{urn:epcglobal:epcis:xsd:1}EPCISDocument":
+            return {"error": "Not EPCISDocument"}, 400
+
+        events = []
+        for child in root:
+            for event_list in child:
+                for event in event_list:
+                    d = ex_xml.map_xml_to_dict(event)
+                    try:
+                        xml_doc = d[event.tag]
+                        epcis_event_obj = event_types[event.tag]()
+                    except Exception:
+                        event_from_xml = ex_xml.find_event_from_xml(event, event_types)
+                        epcis_event_obj = event_types[event_from_xml]
+                        pass
+
+                    xml_dict = ex_xml.map_to_epcis_dict(xml_doc)
+                    ex_xml.map_from_epcis(epcis_event_obj, xml_dict)
+                    events.append({
+                        "_event_time": str(epcis_event_obj._event_time),
+                        "_event_timezone_offset": str(epcis_event_obj._event_timezone_offset),
+                        "_extensions": str(epcis_event_obj._extensions),
+                        "_action": str(epcis_event_obj._action),
+                        "_business_step": str(epcis_event_obj._business_step),
+                    })
+
+
+        return {"success": True, "events": events}, 200
+        
+
 
 JSONView.register(app)
 XMLView.register(app)
