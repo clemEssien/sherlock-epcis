9e4a0e0b463644e47b4aa02890ea2b8d2357936a
Ryan Oostland <ryan.oostland@precise-soft.com>
2021-07-13 16:43:27 -0400

    Create CTEDetector class

--- EPCIS-CTE-Transformation/cte_detector.py
+++ EPCIS-CTE-Transformation/cte_detector.py
@@ -1,8 +1,39 @@
+from JSONDeserialization.epcis_event import EPCISEvent
 import json
+from sys import settrace
 
 
 class CTEDetector:
-    """Provides a class to determine which FDA CTE an EPCIS event corresponds with."""
+    """Class to determine which FDA CTE an EPCIS event corresponds with.
 
-    def __init__(self) -> None:
+    Attributes:
+        cte_list : list
+            List of FDA CTEs that an EPCIS event can correspond to.
+        event_chars : dict
+            A dictionary identifying each FDA CTE by expected characteristics of an EPCIS event.
+    """
+
+    cte_list = [
+        "growing",
+        "growing_sprouts",
+        "creation",
+        "transformation",
+        "shipping",
+        "receiving",
+        "receiving_first_non_fishing_vessel",
+        "receiving_first_fishing_vessel",
+    ]
+
+    def __init__(self, event_chars: dict = None) -> None:
+        self._event_chars: dict = event_chars
+
+    @property
+    def event_chars(self) -> dict:
+        return self._event_chars
+
+    @event_chars.setter
+    def event_chars(self, value: dict):
+        self._event_chars = value
+
+    def detect_cte(epcis_event: EPCISEvent) -> str:
         pass
