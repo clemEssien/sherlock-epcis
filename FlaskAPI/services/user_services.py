273e418a4a904c11911241af89d92992b10e5aad
Charlie Shou <charlie.shou@precise-soft.com>
2021-07-27 01:59:00 -0400

    moved stuff

--- FlaskAPI/services/user_services.py
+++ FlaskAPI/services/user_services.py
@@ -1,7 +1,10 @@
 import hmac
 import hashlib
 
-def create_signature(key: str, message: str) -> str:
+def authenticate_user():    #TODO
+    pass
+
+def create_hash(key: str, message: str) -> str:
     key_bytes = key.encode()
     message_bytes = message.encode()
     return hmac.new(key_bytes, msg=message_bytes, digestmod=hashlib.sha256).hexdigest().upper()
