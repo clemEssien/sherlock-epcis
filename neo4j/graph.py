9f5686bd4645e983e920b3f9f80d9315f8014fa1
Clement <clement.essien@precise-soft.com>
2021-07-30 15:07:54 -0400

    added tests for the graph

--- neo4j/graph.py
+++ neo4j/graph.py
@@ -1,24 +1,10 @@
 import os
 import sys
-from typing import Type
 currentdir = os.path.dirname(os.path.realpath(__file__))
 parentdir = os.path.dirname(currentdir)
 sys.path.append(parentdir)
 
 import db_connect as db_con
-
-global conn 
-conn = None
-
-def connectdb() -> db_con.Neo4jConnection:
-    """method returns a connection to the database"""
-    global conn
-    if not conn:
-        conn = db_con.Neo4jConnection(uri="bolt://localhost:7687", 
-                       user="neo4j",              
-                       password=os.environ['NEO4J_PASSWORD'])
-
-    return conn
 
 class Graph:
     def __init__(self, name) -> None:
@@ -31,36 +17,75 @@ class Graph:
         '''
         self.__name = name
 
-    def graph_exists(self):
+    def graph_exists(self) -> bool:
         '''
         method checks to see if a graph with the provided name already exists
         returns boolean 
         '''
-        query = """
+        cipher_ql = """
                 CALL gds.graph.exists($name) YIELD exists;
         """
-        response = connectdb().query(query,self.__name)   
-        return  response
+        response = connectdb().query(cipher_ql,{"name":self.__name})
+        print(response)
+        if type(response) == list and len(response) ==1:  
+            result = string_btw_xters(str(response),'=','>')
+        return  bool(result)
 
     def create_graph(self):
         '''
         method creates a graph using the name provided in the constructor
         and projects all existing nodes and relationships unto the graph
         '''
-        query = """
-                CALL gds.graph.create($graph_name, '*', '*');
-        """
-        response = connectdb().query(query,{"graph_name":self.__name},None)   
-        return  response
+        try: 
+            if not self.graph_exists():
+                response = False
+            else:
+                cipher_ql = """
+                        CALL gds.graph.create($graph_name, '*', '*');
+                """
+                response = connectdb().query(cipher_ql,{"graph_name":self.__name})
+                response = 'Record nodeProjection' in str(response[0])
+            return  response
+        except Exception as e:
+            return False
+
+        
     
     def remove_graph(self):
         '''
         method removes a graph from the catalog
         '''
-        query = """
-                CALL gds.graph.drop('$graph_name', false) YIELD graphName;
+        cipher_ql = """
+                CALL gds.graph.drop($graph_name, false) YIELD graphName;
                 """
-        return connectdb(query,{"graph_name": self.__name} )
+        response = connectdb().query(cipher_ql,{"graph_name": self.__name} )
+        return response
+
+
+global conn 
+conn = None
+def connectdb() -> db_con.Neo4jConnection:
+    """method returns a connection to the database"""
+    global conn
+    if not conn:
+        conn = db_con.Neo4jConnection(uri="bolt://localhost:7687", 
+                       user="neo4j",              
+                       password=os.environ['NEO4J_PASSWORD'])
+
+    return conn
+
+
+def string_btw_xters(string, initial, terminating)->str:
+    '''
+    method returns the string in between two characters
+    Args:
+        string: str
+        initial character: str
+        terminal character: str
+    '''
+    start = string.find(initial) + len(initial)
+    end = string.find(terminating)
+    return string[start:end]
 
 
  
--- neo4j/tests/graph_test.py
+++ neo4j/tests/graph_test.py
@@ -4,31 +4,22 @@ currentdir = os.path.dirname(os.path.realpath(__file__))
 parentdir = os.path.dirname(currentdir)
 sys.path.append(parentdir)
 
-# print(currentdir)
-print(parentdir)
 import graph as g
 
 graph = g.Graph("new_graph")
 existing_graph_name = "my-graph"
 
 existing_graph = g.Graph(existing_graph_name)
-
-def graph_exists_response(result)-> bool:
-    if type(result) == list:
-        if len(result) == 1:
-            if 'False' in str(result[0]):
-                return True
-            if 'True' in str(result):
-                return False
-    return 
-        
+bool_list = [True, False]
 
 def test_exist_graph():
     response = graph.graph_exists()
-    assert type(graph_exists_response(response)) == bool
-def test_create_graph():
-    response = existing_graph.create_graph()
-    print(response)
-    print(graph_exists_response(response))
+    assert response in bool_list
 
-test_create_graph()
+def test_create_graph():
+    response = graph.create_graph()
+    assert response in bool_list
+    
+def test_delete_graph():
+    response = graph.remove_graph()
+    assert type(response)== list and len(response)<=1 
